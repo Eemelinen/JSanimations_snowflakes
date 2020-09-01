4f753e0c462f98c05bca83b2fd7478aed996a142
Eemeli Surakka <eemeli.surakka@hotmail.fi>
2020-09-01 15:27:26 +0200

    Snowballs created, animated and moving. Not reacting to window size change yet.

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ ctx.canvas.width = window.innerWidth;
 ctx.canvas.height = window.innerHeight;
 let particleArray;
 
-// Particle contructor
+/**
+* ! Particle contructor */
 function Particle(x, y, directionX, directionY, size, color) {
   this.x = x;
   this.y = y;
@@ -14,7 +15,8 @@ function Particle(x, y, directionX, directionY, size, color) {
   this.color = color;
 }
 
-// Draw method for particle. Creting outside constructor saves resources.
+/**
+ * ! Draw method for particle. Creting outside constructor saves resources. */
 Particle.prototype.draw = function() {
   ctx.beginPath();
   ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2, false);
@@ -22,5 +24,46 @@ Particle.prototype.draw = function() {
   ctx.fill();
 }
 
-const particle1 = new Particle(100, 200, 1, 1, 20, 'white');
-particle1.draw();
+/** 
+ * ! Update method for particle */
+Particle.prototype.update = function() {
+  // Check for particle overlapping screen and draw
+  if (this.x + this.size > canvas.width || this.x - this.size < 0) {
+    this.directionX = -this.directionX;
+  }
+  if (this.y + this.size > canvas.height || this.y - this.size < 0) {
+    this.directionY = -this.directionY;
+  }
+  this.x += this.directionX;
+  this.y += this.directionY;
+  this.draw();
+}
+
+/** 
+ * ! Init function to create particles. */
+function init() {
+  particleArray = [];
+  for (let i = 0; i < 100; i++) {
+    let size = Math.random() * 20;
+    let x = Math.random() * (innerWidth - size * 2);
+    let y = Math.random() * (innerHeight - size * 2);
+    let directionX = (Math.random() * .4) - .2;
+    let directionY = (Math.random() * .4) - .2;
+    let color = 'white';
+    particleArray.push(new Particle(x, y, directionX, directionY, size, color));
+  }
+}
+
+/** 
+ * ! Animation loop function to move particles. */
+function animate() {
+  requestAnimationFrame(animate);
+  ctx.clearRect(0, 0, innerWidth, innerHeight);
+
+  for (let i = 0; i < particleArray.length; i++) {
+    particleArray[i].update();
+  }
+}
+
+init();
+animate();
